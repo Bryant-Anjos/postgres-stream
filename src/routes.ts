7b1e72104c67918f373d52cf298687d26560a366
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2021-07-28 08:23:08 -0300

    feat(routes): route  to directly download without generate csv created

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,27 @@ type CountableTransform = Transform & Record<'counter', number>
 
 const router = Router()
 
+const writableMapToCSV = new Transform({
+  transform(chunk, encoding, cb) {
+    ;(this as CountableTransform).counter =
+      (this as CountableTransform).counter ?? 0
+
+    try {
+      const string = chunk.toString('utf8')
+      const data = JSON.parse(string)
+      const headers =
+        (this as CountableTransform).counter === 0
+          ? `${Object.keys(data).join(',')}\n`
+          : ''
+      const row = `${headers}${Object.values(data).join(',')}\n`
+      ;(this as CountableTransform).counter += 1
+      cb(null, row)
+    } catch (err) {
+      cb(null, '')
+    }
+  },
+})
+
 router.get('/', (req, res) => {
   res.json({ ok: true })
 })
@@ -25,36 +46,29 @@ router.get('/sales/export', async (req, res) => {
   const ws = fs.createWriteStream(csv, { encoding: 'utf8' })
   const sales = db.select('*').from('sales').stream()
 
-  const writableMapToCSV = new Transform({
-    transform(chunk, encoding, cb) {
-      ;(this as CountableTransform).counter =
-        (this as CountableTransform).counter ?? 0
-
-      try {
-        const string = chunk.toString('utf8')
-        const data = JSON.parse(string)
-        const headers =
-          (this as CountableTransform).counter === 0
-            ? `${Object.keys(data).join(',')}\n`
-            : ''
-        const row = `${headers}${Object.values(data).join(',')}\n`
-        ;(this as CountableTransform).counter += 1
-        cb(null, row)
-      } catch (err) {
-        cb(null, '')
-      }
-    },
-  })
-
   sales
     .pipe(Stringify())
     .pipe(writableMapToCSV)
-    .on('data', console.log)
     .pipe(ws)
     .on('close', () => {
       res.type('text/csv; charset=utf-8')
       res.attachment(path.basename(csv)).sendFile(csv)
     })
+    .on('error', (error) => {
+      res.json({ error })
+    })
+})
+
+router.get('/sales/download', async (req, res) => {
+  res.type('text/csv; charset=utf-8')
+  res.attachment(path.basename('out.csv'))
+
+  const sales = db.select('*').from('sales').stream()
+
+  sales
+    .pipe(Stringify())
+    .pipe(writableMapToCSV)
+    .pipe(res)
     .on('error', (error) => {
       res.json({ error })
     })
